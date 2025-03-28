f5c0129f8710d1244bad68f1c89a6948452033da
Dryn2007 <adrianadiputra2007@gmail.com>
2025-03-28 23:14:04 +0700

    update1

---
+++ prisma/migrations/20250328161155_/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `clerkId` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "User_clerkId_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "clerkId",
+DROP COLUMN "name",
+ADD COLUMN     "password" TEXT NOT NULL DEFAULT '';
